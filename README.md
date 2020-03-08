4850f2b0e5b2867fbc4ecb672ae2f9644347bf1a
mkesicki <marc_he_w@poczta.onet.pl>
2020-03-08 17:50:55 +0100

    Update readme and schema

--- README.md
+++ README.md
@@ -1,24 +1,21 @@
-# Lumen PHP Framework
+#Install
 
-[![Build Status](https://travis-ci.org/laravel/lumen-framework.svg)](https://travis-ci.org/laravel/lumen-framework)
-[![Total Downloads](https://poser.pugx.org/laravel/lumen-framework/d/total.svg)](https://packagist.org/packages/laravel/lumen-framework)
-[![Latest Stable Version](https://poser.pugx.org/laravel/lumen-framework/v/stable.svg)](https://packagist.org/packages/laravel/lumen-framework)
-[![License](https://poser.pugx.org/laravel/lumen-framework/license.svg)](https://packagist.org/packages/laravel/lumen-framework)
+ - Clone repository
+ ```git clone https://github.com/mkesicki/todo.git```
 
-Laravel Lumen is a stunningly fast PHP micro-framework for building web applications with expressive, elegant syntax. We believe development must be an enjoyable, creative experience to be truly fulfilling. Lumen attempts to take the pain out of development by easing common tasks used in the majority of web projects, such as routing, database abstraction, queueing, and caching.
+ - Run composer install in repository folder
+ ```composer install```
 
-## Official Documentation
+- Create and configure .env file with database name and permissions
 
-Documentation for the framework can be found on the [Lumen website](https://lumen.laravel.com/docs).
+- Migrate database
+```php artisan migrate```
 
-## Contributing
+- Optionally insert test data
+```mysql -uUser -pPassword DatabaseName < schema.sql```
 
-Thank you for considering contributing to Lumen! The contribution guide can be found in the [Laravel documentation](https://laravel.com/docs/contributions).
+- Start local server
+```php -S localhost:8000 -t public```
 
-## Security Vulnerabilities
-
-If you discover a security vulnerability within Lumen, please send an e-mail to Taylor Otwell at taylor@laravel.com. All security vulnerabilities will be promptly addressed.
-
-## License
-
-The Lumen framework is open-sourced software licensed under the [MIT license](https://opensource.org/licenses/MIT).
+- To run tests
+```vendor\bin\phpunit``` 
--- schema.sql
+++ schema.sql
@@ -1,9 +1,5 @@
+INSERT INTO `users`(`id`, `created_at`, `updated_at`, `firstname`, `lastname`, `email`, `birthdate`, `phone`, `password`, `gender`, `api_key`) VALUES (1, '2020-03-08 17:28:28', '2020-03-08 17:28:37', 'John', 'doe', 'john@example.com', '1983-06-05', '123456789', '$2y$10$rusbnlpi16kUxyPL/j7vTumx8NSjUFEqTZVOZ5gjKPwZJR4ULHCvu', 'male', 'TWNvYU4zTlJHQWFmUXRKRzBVa2c0NE9INzhsTUZPSTdXdTN0Y2VPU2tmMDA1T3BWbktoa0FTYmlNRHYwTnIwZA==');
 INSERT INTO `categories`(`id`, `created_at`, `updated_at`, `name`, `description`) VALUES (1, '2020-03-08 17:28:43', '2020-03-08 17:28:43', 'new category', 'desc');
 INSERT INTO `categories`(`id`, `created_at`, `updated_at`, `name`, `description`) VALUES (3, '2020-03-08 17:30:36', '2020-03-08 17:30:36', 'home', 'home stuff');
 INSERT INTO `lists`(`id`, `created_at`, `updated_at`, `user_id`, `date`, `name`, `description`) VALUES (1, '2020-03-08 17:30:50', '2020-03-08 17:30:50', 1, '2020-03-18 00:00:00', 'shoping list', 'Things to buy');
-INSERT INTO `migrations`(`id`, `migration`, `batch`) VALUES (5, '2020_03_07_202328_create_table_users', 1);
-INSERT INTO `migrations`(`id`, `migration`, `batch`) VALUES (6, '2020_03_07_203156_create_table_categories', 1);
-INSERT INTO `migrations`(`id`, `migration`, `batch`) VALUES (7, '2020_03_08_093926_create_table_lists', 1);
-INSERT INTO `migrations`(`id`, `migration`, `batch`) VALUES (8, '2020_03_08_094408_create_table_tasks', 1);
 INSERT INTO `tasks`(`id`, `created_at`, `updated_at`, `category_id`, `list_id`, `date`, `status`, `name`, `description`) VALUES (1, '2020-03-08 17:31:47', '2020-03-08 17:31:47', 3, 1, '2020-03-18 00:00:00', 'Snoozed', 'Buy a dog food', 'This furry thing needs to eat');
-INSERT INTO `users`(`id`, `created_at`, `updated_at`, `firstname`, `lastname`, `email`, `birthdate`, `phone`, `password`, `gender`, `api_key`) VALUES (1, '2020-03-08 17:28:28', '2020-03-08 17:28:37', 'John', 'doe', 'john@example.com', '1983-06-05', '123456789', '$2y$10$rusbnlpi16kUxyPL/j7vTumx8NSjUFEqTZVOZ5gjKPwZJR4ULHCvu', 'male', 'TWNvYU4zTlJHQWFmUXRKRzBVa2c0NE9INzhsTUZPSTdXdTN0Y2VPU2tmMDA1T3BWbktoa0FTYmlNRHYwTnIwZA==');
